082673dbd75b070815d1a07c530717ecdcb50403
Kanfadon <kanfadon@mail.ru>
2021-03-07 02:31:10 +0300

    complete UI

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,18 +7,23 @@ import AddPanel from './../add-panel';
 export default class App extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
+            category: 'all',
             startId: 10,
             filter: '',
+
+            // #################################################
+            // TODO: реализовать хранение  через сервер
             itemList: [
-                {id: 1, text: 'супер длииииииииииииииииинннноееееее и вааааааажноееееееееее дело', important: true, complete: false},
-                {id: 2, text: 'купить хлеб', important: false, complete: true},
-                {id: 3, text: 'прочитать книгу', important: false, complete: false},
-                {id: 4, text: 'вынести мусор', important: false, complete: false},
-                {id: 5, text: 'полгладить кота', important: false, complete: false},
-                {id: 6, text: 'постирать вещи', important: false, complete: false}
+                { id: 1, text: 'супер длииииииииииииииииинннноееееее и вааааааажноееееееееее дело', important: true, complete: false },
+                { id: 2, text: 'купить хлеб', important: false, complete: true },
+                { id: 3, text: 'прочитать книгу', important: false, complete: false },
+                { id: 4, text: 'вынести мусор', important: false, complete: false },
+                { id: 5, text: 'полгладить кота', important: false, complete: false },
+                { id: 6, text: 'постирать вещи', important: false, complete: false }
             ]
+            // #################################################
         }
 
         this.onImportant = this.onImportant.bind(this);
@@ -26,95 +31,121 @@ export default class App extends Component {
         this.addItem = this.addItem.bind(this);
         this.onComplete = this.onComplete.bind(this);
         this.onFilterChange = this.onFilterChange.bind(this);
+        this.selectCategory = this.selectCategory.bind(this);
+        this.changeCategory = this.changeCategory.bind(this);
+    }
+
+    selectCategory(category) {
+        this.setState({ category: category });
+    }
+
+    changeCategory(array) {
+        switch (this.state.category) {
+            case 'all':
+                return array;
+            case 'important':
+                return array.filter(elem => elem.important);
+            case 'complete':
+                return array.filter(elem => elem.complete);
+            default:
+                return array;
+        }
     }
 
     onImportant(id) {
-        this.setState(({itemList}) => {
+        this.setState(({ itemList }) => {
             const index = itemList.findIndex(elem => elem.id === id);
-            const newItem = {...itemList[index], important: !itemList[index].important};
+            const newItem = { ...itemList[index], important: !itemList[index].important };
 
-            return {itemList: [
-                ...itemList.slice(0, index),
-                newItem,
-                ...itemList.slice(index+1)]
+            return {
+                itemList: [
+                    ...itemList.slice(0, index),
+                    newItem,
+                    ...itemList.slice(index + 1)]
             };
-        })
+        });
     }
 
     onDelete(id) {
-        this.setState(({itemList}) => {
+        this.setState(({ itemList }) => {
             const index = itemList.findIndex(elem => elem.id === id);
             const before = itemList.slice(0, index);
-            const after = itemList.slice (index+1, itemList.length);
+            const after = itemList.slice(index + 1, itemList.length);
 
-            return {itemList: [...before, ...after]};
+            return { itemList: [...before, ...after] };
         })
     }
 
     addItem(text) {
-        this.setState(({startId, itemList}) => {
-            return {startId: ++startId, itemList: [
-                ...itemList,
-                {id: ++startId, text: text, important: false, complite:false}
-            ]}
+        this.setState(({ startId, itemList }) => {
+            return {
+                startId: ++startId, itemList: [
+                    ...itemList,
+                    { id: ++startId, text: text, important: false, complite: false }
+                ]
+            }
         })
     }
 
     onFilterChange(newFilter) {
-        this.setState({filter: newFilter})
+        this.setState({ filter: newFilter })
     }
 
-    onFilter() {
+    onFilter(array) {
         if (this.state.filter) {
-            const newArray = this.state.itemList.filter(elem => elem.text.toLowerCase().includes(this.state.filter));
+            const newArray = array.filter(elem => elem.text.toLowerCase().includes(this.state.filter));
             return newArray;
         } else {
-            return this.state.itemList;
+            return array;
         }
     }
 
     onComplete(id) {
-        this.setState(({itemList}) => {
+        this.setState(({ itemList }) => {
             const index = itemList.findIndex(elem => elem.id === id);
-            const newItem = {...itemList[index], complete: !itemList[index].complete};
+            const newItem = { ...itemList[index], complete: !itemList[index].complete };
 
-            return {itemList: [
-                ...itemList.slice(0, index),
-                newItem,
-                ...itemList.slice(index+1)]
+            return {
+                itemList: [
+                    ...itemList.slice(0, index),
+                    newItem,
+                    ...itemList.slice(index + 1)]
             };
         })
     }
 
     render() {
-        const { itemList } = this.state;
-        const filtered = this.onFilter();
+        const filteredCategory = this.changeCategory(this.state.itemList)
+        const filtered = this.onFilter(filteredCategory);
+
+
 
         const itemCount = filtered.length;
         const completeCount = filtered.filter(elem => elem.complete).length;
 
-        
-
         return (
 
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8 offset-md-2">
-                    <AppHeader
-                    todos={itemCount}
-                    done={completeCount}
-                    />
-                    <SearchPanel onFilterChange={this.onFilterChange} />
-                    <TodoList 
-                    onImportant={this.onImportant}
-                    onComplete={this.onComplete}
-                    itemList={filtered}
-                    onDelete={this.onDelete}
-                    />
-                    <AddPanel addItem={this.addItem}/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 offset-md-2">
+                        <AppHeader
+                            todos={itemCount}
+                            done={completeCount}
+                        />
+                        <SearchPanel
+                            onFilterChange={this.onFilterChange}
+                            selectCategory={this.selectCategory}
+                        />
+                        <TodoList
+                            onImportant={this.onImportant}
+                            onComplete={this.onComplete}
+                            itemList={filtered}
+                            onDelete={this.onDelete}
+                        />
+                        <AddPanel addItem={this.addItem} />
+                    </div>
                 </div>
             </div>
-        </div>
         )
     }
 }
